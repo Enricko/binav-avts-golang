027e1c084e68a701f11eddd5a204ae86d6938f38
enricko <enricko.putra028@gmail.com>
2024-05-12 21:41:49 +0700

    chore: Add user data to penduduk.html template

--- app/controllers/penduduk.go
+++ app/controllers/penduduk.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"fmt"
+	"golang-app/app/models"
+	"golang-app/database"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
 )
 
 type PendudukController struct {
@@ -17,8 +21,19 @@ func NewPendudukController() *PendudukController {
 }
 func (r *PendudukController) Index(c *gin.Context) {
 	// Data to pass to the index.html template
+
+	var users []models.User
+
+	// Retrieve all users from the database
+	err := database.DB.Find(&users).Error
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	app := gin.H{
 		"title": "Dashboard Panel",
+		"users": users,
 	}
 
 	c.HTML(http.StatusOK, "penduduk.html", app)
