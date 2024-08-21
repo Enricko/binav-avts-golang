13e27ddbda4cc4001ff7174ec44bb1d6ff47daf9
fadillarizky <fadillarizky294@gmail.com>
2024-08-21 13:53:51 +0700

    add reset password

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,5 +1,6 @@
 const loginForm = document.getElementById("login-form");
 const buttonLogin = document.getElementById("btn-login");
+const titleAlert = document.getElementById("title-alert");
 
 loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -23,41 +24,179 @@ loginForm.addEventListener("submit", async (event) => {
         })
         .then((data) => {
           console.log(data);
-          buttonLogin.innerHTML = 'Login';
-            // alert("Login successful!");
-            
+          buttonLogin.innerHTML = originalText;
             window.location.href = "/";
-            // Save token or perform other actions like redirecting
             console.log(data.token);
-            
             localStorage.setItem("token", data.token); // Example of saving JWT token
         })
         .catch((error) => {
           console.error("Error:", error);
-          buttonLogin.innerHTML = 'Login';
-          // alert("Error!");
+          buttonLogin.innerHTML = originalText;
+          const errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+          titleAlert.innerHTML = `Invalid Email or password. Please try again.`;
+        errorModal.show();
           
         });
     }
 
-  
- 
 }
 );
 
-// async function login(email, password) {
-//     const url = "https://api.binav-avts.id:5000/api/auth/login";
-//     const data = { email, password };
+const forgotPassForm = document.getElementById("forgot-password-form");
+const buttonForgot = document.getElementById("btn-forgot");
+let email;
 
-//     const response = await fetch(url, {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(data),
-//     });
+function showOtpForm() {
+    document.getElementById('forgotPasswordForm').style.display = 'none';
+    document.getElementById('OtpForm').style.display = 'block';
+    email = document.getElementById('forgotEmail').value;
+    document.getElementById('otpSubtitle').innerHTML = `We've sent a code to <strong>${email}</strong>`;
+}
 
-//     if (!response.ok) {
-//       throw new Error(`Login failed: ${response.status}`);
-//     }
+forgotPassForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+    console.log("forgot pass");
+    if (forgotPassForm.checkValidity()) {
+      const form = new FormData(forgotPassForm);
 
-//     return await response.json();
-//   }
+      const originalText = buttonForgot.textContent;
+      buttonForgot.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+
+      fetch("forgot-password", {
+        method: "POST",
+        body: form,
+      })
+        .then(response => {
+          if (!response.ok) {
+              return response.json().then(err => { throw err; });
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          buttonForgot.innerHTML = originalText;
+          showOtpForm();
+          forgotPassForm.reset();
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          buttonForgot.innerHTML = originalText;
+          const errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+          titleAlert.innerHTML = `Error `;
+        errorModal.show();
+          
+        });
+    }
+
+}
+);
+
+const otpForm = document.getElementById("otp-form");
+const buttonSubmitOtp = document.getElementById("btn-submit-otp");
+let otp = "";
+
+function showResetPassForm() {
+    document.getElementById('OtpForm').style.display = 'none';
+    document.getElementById('ResetPassForm').style.display = 'block';
+}
+
+otpForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+    if (otpForm.checkValidity()) {
+      
+
+            for (let i = 1; i <= 6; i++) {
+                const otpInput = document.getElementById("otp" + i);
+                otp += otpInput.value;
+            }
+      const form = new FormData(otpForm);
+      form.append('email',email);
+      form.append('otp',otp);
+
+      const originalText = buttonSubmitOtp.textContent;
+      buttonSubmitOtp.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+
+      fetch("validate-otp", {
+        method: "POST",
+        body: form,
+      })
+        .then(response => {
+          if (!response.ok) {
+              return response.json().then(err => { throw err; });
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          buttonSubmitOtp.innerHTML = originalText;
+          console.log("Success");
+          showResetPassForm();
+          otpForm.reset();
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          buttonSubmitOtp.innerHTML = originalText;
+          const errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+          titleAlert.innerHTML = `Invalid OTP`;
+        errorModal.show();
+          
+        });
+    }
+
+}
+);
+
+const resetPassForm = document.getElementById("reset-pass-form");
+const buttonResetPass = document.getElementById("btn-reset-pass");
+
+function showLoginForm() {
+  document.getElementById('ResetPassForm').style.display = 'none';
+  document.getElementById('loginForm').style.display = 'block';
+}
+
+resetPassForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+    if (resetPassForm.checkValidity()) {
+      
+      const form = new FormData(resetPassForm);
+      form.append('email',email);
+      form.append('otp',otp);
+
+      const originalText = buttonResetPass.textContent;
+      buttonResetPass.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+
+      fetch("reset-password", {
+        method: "POST",
+        body: form,
+      })
+        .then(response => {
+          if (!response.ok) {
+              return response.json().then(err => { throw err; });
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          buttonResetPass.innerHTML = originalText;
+          console.log("Success");
+          showLoginForm();
+          const successModal = new bootstrap.Modal(document.getElementById('successModal'));
+          successModal.show();
+          resetPassForm.reset();
+
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          buttonResetPass.innerHTML = originalText;
+          const errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+          titleAlert.innerHTML = error.message;
+        errorModal.show();
+          
+        });
+    }
+
+}
+);
