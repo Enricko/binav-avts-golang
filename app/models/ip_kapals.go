eff9c8ceea9c36035eab1b3950d4c9438b05fdb6
fadillarizky <fadillarizky294@gmail.com>
2024-08-29 15:20:01 +0700

    add insert ip

--- app/models/ip_kapals.go
+++ app/models/ip_kapals.go
@@ -2,16 +2,15 @@ package models
 
 import (
 	"time"
-
 )
 
 type TypeIP string
 
 const (
-	ALL TypeIP = "all"
-	GGA TypeIP = "gga"
-	HDT TypeIP = "hdt"
-	VTG TypeIP = "vtg"
+	ALL   TypeIP = "all"
+	GGA   TypeIP = "gga"
+	HDT   TypeIP = "hdt"
+	VTG   TypeIP = "vtg"
 	DEPTH TypeIP = "depth"
 )
 
@@ -20,9 +19,8 @@ type IPKapal struct {
 	CallSign  string    `gorm:"not null;index" json:"call_sign" binding:"required"`
 	TypeIP    TypeIP    `gorm:"type:enum('all','gga','hdt','vtg','depth');not null" json:"type_ip" binding:"required"`
 	IP        string    `gorm:"type:varchar(16);not null;" json:"ip" binding:"required"`
-	Port      uint16    `json:"port;not null;" binding:"required"`
+	Port      uint16    `json:"port" binding:"required"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Kapal     *Kapal    `gorm:"foreignKey:CallSign;association_foreignkey:CallSign"`
-
 }
